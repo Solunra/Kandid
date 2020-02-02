6f3366bedaaa3b1229b5991d03357ed2c4090ed4
Sunny <sunnydeng90@gmail.com>
2020-02-01 21:25:07 -0500

    Initial Creation of Test Cases

---
+++ CoreTests/Test.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    public class Tests
+    {
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
