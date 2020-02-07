6a7f383d83f153c608d55263f68f9185237aaeb5
Solunra <sunnydeng90@gmail.com>
2020-02-07 15:26:11 -0500

    Added proper tests

--- test/dummyTest.js
+++ test/dummyTest.js
@@ -1,5 +1,7 @@
 var assert = require('chai').assert;
 
-assert.isTrue(true, "True is true");
-assert.equal(true, true, "True is equal to true");
-//assert.equal(true, false);
+it("dummyTests", () => {
+    assert.isTrue(true, "True is true");
+    assert.equal(true, true, "True is equal to true");
+    //assert.equal(true, false);
+});
