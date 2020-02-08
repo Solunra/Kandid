6680e100de630ac88f89a2d2436ef49bb4701a46
Sunny <sunnydeng90@gmail.com>
2020-02-08 13:10:35 -0500

    Added db startup and schemas

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send("Web Page Test"));
 
-app.listen(port, () => console.log(`Web page started on port ${port}`))
+app.listen(port, () => console.log(`Web page started on port ${port}`));
