a25f9f37aead82e202f6354b5fc24707751a8e35
Sunny <sunnydeng90@gmail.com>
2020-02-09 20:03:17 -0500

    Added PUT post

--- server/routes/post.js
+++ server/routes/post.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 module.exports = router;
 
+const postModel = mongoose.model("Post");
+
 router.get("/", (req, res) => {
     Post.find().sort('-PostDate').exec((err, post) => {
         if (err) {
@@ -17,12 +19,21 @@ router.get("/", (req, res) => {
 });
 
 router.get("/test", (req, res) => {
-    const postModel = mongoose.model("Post");
     const post0 = new postModel;
-    post0.UserId = "test";
+    post0.UserID = "test";
     post0.ImageLink = "test";
     post0.Caption = "Testing this caption~ XOXO";
     post0.Like = 20;
     post0.save();
     res.send("[Database has obtained a post]")
+});
+
+router.put("/", (req, res) => {
+    var post = new postModel;
+    post.UserID = req.user.UserID;
+    post.ImageLink = req.body.post.ImageLink;
+    post.Caption = req.body.post.Caption;
+    post.Like = 0;
+
+    res.status(200).send("Post succesfully posted");
 });
