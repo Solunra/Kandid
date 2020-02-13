9fb3446950c7bbb05e072f4302b63253484e017f
unknown <ashphil30@gmail.com>
2020-02-13 15:51:38 -0500

    adding some corrections

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -9,11 +9,20 @@ module.exports = router;
 const postModel = mongoose.model("Comment");
 
 router.get("/", (req, res) => {
-    Post.find().sort('-PostDate').exec((err, post) => {
+    Comment.find().sort('-PostDate').exec((err, post) => {
         if (err) {
-            res.status(200).send({message: "There are no posts"});
+            res.status(200).send({message: "There are no comments"});
         } else {
             res.status(200).send({posts: post});
         }
     });
+});
+
+router.put("/", (req, res) => {
+    var comment = new commentModel;
+    comment.UserID = req.user.UserID;
+    comment.PostID = req.user.PostID;
+    comment.Comment = req.body.post.Comment;
+    comment.save();
+    res.status(200).send("Comment successfully posted");
 });
