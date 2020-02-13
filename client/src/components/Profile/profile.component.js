2cb9c49d76d6ae24a1eedebb447d6e980413dc27
Juan Hoyos <juanse.jsh@gmail.com>
2020-02-13 02:47:18 -0500

    Added the NavBar for the wall. Adjusting the styles a little bit

---
+++ client/src/components/Profile/profile.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import NavBar from "./NavBar";
+
+export default class Profile extends Component{
+    render(){
+        return(
+           <NavBar>
+           </NavBar>
+        );
+    }
+}
