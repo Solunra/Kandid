5acd245535fd3e168c01144a12bb472a9bbcd05c
Juan Hoyos <juanse.jsh@gmail.com>
2020-02-13 09:58:54 -0500

    added possible post icon

--- client/src/components/Profile/profile.component.js
+++ client/src/components/Profile/profile.component.js
@@ -3,8 +3,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import NavBar from "./NavBar";
+import './index.cssgti '
+
 
 export default function SimpleContainer(){
+    const handlePost = event =>{
+        console.log('Post Picture');
+    }
         return (
             <React.Fragment>
                 <NavBar/>
@@ -12,6 +17,7 @@ export default function SimpleContainer(){
                 <Container maxWidth="lg">
                     <Typography component="div" style={{backgroundColor: '#f2f2f2', height: '100vh'}}/>
                 </Container>
+                <button id="PostPicture" onClick={handlePost}/>
             </React.Fragment>
         );
 }
