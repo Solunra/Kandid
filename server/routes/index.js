7007e5e0555334885c92c59ea3feac3bca3c1363
unknown <ashphil30@gmail.com>
2020-02-13 11:57:09 -0500

    Updated routes and created comment service

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const post = require('./post');
+const comment = require('./comment');
 
 const router = express.Router();
 
 router.use('/api/post', post);
+router.use('/api/login', comment);
 
 module.exports = router;
