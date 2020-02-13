bf798784d0cf56615268bb89aa47ff19e18a54a2
Juan Hoyos <juanse.jsh@gmail.com>
2020-02-13 11:21:30 -0500

    added post icon

--- client/src/components/Profile/profile.component.js
+++ client/src/components/Profile/profile.component.js
@@ -3,7 +3,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import NavBar from "./NavBar";
-import './index.cssgti '
+import './index.css'
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import IconButton from "@material-ui/core/IconButton";
 
 
 export default function SimpleContainer(){
@@ -13,11 +15,22 @@ export default function SimpleContainer(){
         return (
             <React.Fragment>
                 <NavBar/>
-                <CssBaseline/>
+                <div>
                 <Container maxWidth="lg">
                     <Typography component="div" style={{backgroundColor: '#f2f2f2', height: '100vh'}}/>
                 </Container>
-                <button id="PostPicture" onClick={handlePost}/>
+                <div style={{position:'sticky'}}>
+                <IconButton
+                    aria-label="show more"
+                    aria-controls={'primary-search-account-menu-mobile'}
+                    aria-haspopup="true"
+                    onClick={handlePost}
+                    color="inherit"
+                >
+                    <AddAPhotoIcon fontSize="large" />
+                </IconButton>
+                </div>
+                </div>
             </React.Fragment>
         );
 }
