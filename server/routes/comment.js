f16ff54728daca49535a3bff5d940a0def94cf3e
unknown <ashphil30@gmail.com>
2020-02-13 11:59:52 -0500

    Fixed small error in comment.js

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { Comment } = require('../database/schemas');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
