57a331412e27e2cd651fb711d0953ea26c38b574
jshoyos <59665374+jshoyos@users.noreply.github.com>
2020-02-14 13:13:55 -0500

    Task/53 (#59)
    * trying to connect with the database
    
    * figuring out the connection problem
    
    * connection to database
    
    * Added UI Component
    
    * View where the pictures will appear when posted. Getting the pictures from the database. The images from local files are not being shown only images from the web work for now
    
    * Posting a picture from the web into the wall. For now the caption is hard coded
    
    * Changed  the backend to receive a link instead of a file
    
    Co-authored-by: Sunny Deng <43704440+Solunra@users.noreply.github.com>

--- client/src/components/Profile/profile.component.js
+++ client/src/components/Profile/profile.component.js
@@ -1,62 +1,59 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, {Component} from 'react';
 import NavBar from "./NavBar";
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 import './index.css'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 import IconButton from "@material-ui/core/IconButton";
+import request from 'superagent';
+import Post from './Post';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        width: 500,
-        height: 450,
-    },
-}));
-
-let projects = [{
-    id:["0","1"],
-    photo:["../images/camera.png","../images/pngtree-vector-camera-icon-png-image_926156.jpg"],
-    text:["hello","Byeee"]
-},
-    {
-        text:["Project number one", "Project number two", "Project number 3"],
-    }];
-export default function SimpleContainer(){
-    const handlePost = event =>{
-        console.log('Post Picture');
+export default class Profile extends Component{
+    constructor(props) {
+        super(props);
+        this.handlePost=this.handlePost.bind(this);
+        this.state = {
+            posts: []
+        };
+    };
+    // UserID: { type: String },
+    // ImageLink: { type: String },
+    // Caption: { type: String },
+    // Like: { type: Number },
+    handlePost = (event) => {
+        console.log("posting");
+        const link=prompt("Enter the link of the picture you which to post:");
+        console.log(link);
+        let json ={post:{ImageLink:link,Caption:"It's been a while since my last post"}};
+        console.log(json);
+        request.put('http://localhost:8000/api/post')
+            .send(json)
+            .then(res=>{console.log(res)});
+    };
+    componentDidMount() {
+        // const response = fetch('http://localhost:8000/api/post',{
+        //     method:'GET'
+        // });
+        request.get('http://localhost:8000/api/post')
+            .then(res => res.body.posts)
+            .then(data => {
+                console.log(data);
+                this.setState({posts: data});
+                console.log(this.state)
+            });
     }
-    const classes = useStyles();
+
+    render(){
         return (
             <React.Fragment>
                 <NavBar/>
                 <div className="container">
-                    <div className="images">
-                        <GridList cellHeight={160} className={classes.gridList} cols={1}>
-                            {projects.map(tile => (
-                                <GridListTile key={tile.id} cols={1}>
-                                    <img src={tile.photo} width="500px" height="500px"/>
-                                </GridListTile>
-                            ))}
-                        </GridList>
-                    </div>
+                    <Post />
                 </div>
                 <div id="PostIcon">
                     <IconButton
                         aria-label="show more"
                         aria-controls={'primary-search-account-menu-mobile'}
                         aria-haspopup="true"
-                        onClick={handlePost}
+                        onClick={this.handlePost}
                         color="inherit"
                     >
                         <AddAPhotoIcon fontSize="large" />
@@ -64,4 +61,5 @@ export default function SimpleContainer(){
                 </div>
             </React.Fragment>
         );
+    }
 }
