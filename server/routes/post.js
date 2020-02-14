1bf19b4d0ef34b67eb37ebc037dcef786648c7b8
Juan Hoyos <juanse.jsh@gmail.com>
2020-02-14 13:12:14 -0500

    Changed  the backend to receive a link instead of a file

--- server/routes/post.js
+++ server/routes/post.js
@@ -36,17 +36,18 @@ router.get("/test", (req, res) => {
 
 router.put("/", (req, res) => {
     var post = new postModel;
-    post.UserID = req.user.UserID;
-
+    // post.UserID = req.user.UserID;
+    console.log(req.body);
     post.Caption = req.body.post.Caption;
     post.Like = 0;
-    var imageFile = req.files.image;
-    post.ImageLink = uploadPath + imageFile.name;
-    imageFile.mv(post.ImageLink, (err) => {
-       if (err) {
-           res.status(500).send(err);
-       }
-    });
+    // var imageFile = req.files.image;
+    // post.ImageLink = uploadPath + imageFile.name;
+    // imageFile.mv(post.ImageLink, (err) => {
+    //    if (err) {
+    //        res.status(500).send(err);
+    //    }
+    // });
+    post.ImageLink=req.body.post.ImageLink;
     post.save();
     res.status(200).send("Post succesfully posted");
 });
