0543525952f5e1d001ce6517eb672f634b5fb4f4
Tigran Karapetyan <tigran.karapetyan@ericsson.com>
2020-02-26 14:42:59 -0500

    implemented user sessions, waiting for login to be complete

--- client/src/components/LoginSignUp/login.component.js
+++ client/src/components/LoginSignUp/login.component.js
@@ -1,76 +1,121 @@
-import React,{Component} from "react";
-import {Link, Route, Switch} from "react-router-dom";
-import './index.css';
-import request from 'superagent';
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import "./index.css";
+import request from "superagent";
 
-export default  class Login extends Component{
-    constructor(props) {
-        super(props);
-        this.state={
-            email:"",
-            password:"",
-            rememberMe:false
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleSubmit=(event)=>{
-        event.preventDefault();
-        let json=JSON.stringify(this.state);
-        console.log(json);
-        request
-            .put("http://localhost:8000/api/login")
-            .send({profile: json})
-            .end((err,res) => {
-            });
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false
     };
-    handleChange(event){
-        this.setState({[event.target.name]:event.target.value});
-    }
-    render() {
-        return(
-            <div className="App">
-            <form onSubmit={this.handleSubmit}>
-                <nav className="navbar-expand-sm navbar-light fixed-top">
-                    <div className="container">
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={"/login"}>Login</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <h3>Sign In</h3>
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    const rememberMe = localStorage.getItem("rememberMe") === "true";
+    const email = rememberMe ? localStorage.getItem("email") : "";
+    this.setState({ [email]: email, [rememberMe]: rememberMe });
+  }
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" name="email" onChange={this.handleChange}/>
-                </div>
+  handleSubmit = event => {
+    const { email, password, rememberMe } = this.state;
+    localStorage.setItem("rememberMe", rememberMe);
+    localStorage.setItem("email", rememberMe ? email : "");
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" name="password" onChange={this.handleChange}/>
-                </div>
+    event.preventDefault();
+    let json = JSON.stringify(this.state);
+    console.log(json);
+    request
+      .put("http://localhost:8000/api/login")
+      .send({ profile: json })
+      .end((err, res) => {});
+  };
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" onChange={e=>this.setState({[this.state.rememberMe]:!this.state.rememberMe})}/>
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+  handleChange = event => {
+    const input = event.target;
+    const value = input.type === "checkbox" ? input.checked : input.value;
 
-                <button type="submit" className="btn btn-primary btn-block">Login</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
+    this.setState({ [input.name]: value });
+    //this.setState({ [event.target.name]: event.target.value });
+  };
+  componentDidMount() {}
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <nav className="navbar-expand-sm navbar-light fixed-top">
+            <div className="container">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarTogglerDemo02"
+              >
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/login"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-        );
-    }
+          </nav>
+          <h3>Sign In</h3>
+
+          <div className="form-group">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              name="email"
+              value={this.state.user}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input
+                name="rememberMe"
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+                checked={this.state.rememberMe}
+                onChange={this.handleChange}
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-block">
+            Login
+          </button>
+          <p className="forgot-password text-right">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
