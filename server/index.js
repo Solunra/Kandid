35c59075bb7f26fbb12fb78d47f0a0866f9a789e
Sunny <sunnydeng90@gmail.com>
2020-02-27 20:38:13 -0500

    Added file upload

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const app = express();
 const port = 8000;
 
@@ -8,7 +9,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-})
+});
 const routes = require('./routes/index');
 
 // TODO remove this once the front end has been implemented
@@ -19,5 +20,8 @@ app.use('/', routes);
 
 // Allows static images to be placed in the /images folder
 app.use('/images', express.static(__dirname + '/images'));
+
+// Allows file upload
+app.use(fileUpload());
 
 app.listen(port, () => console.log(`Web page started on port ${port}`));
