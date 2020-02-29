5263929d762558eb679fb492773d53ca337d4798
Sunny <sunnydeng90@gmail.com>
2020-02-29 10:13:05 -0500

    Finish updating branch

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const cors = require('cors');
 
 const post = require('./post');
+const comment = require('./comment');
 const login = require('./login');
 const register = require('./register');
 
 const router = express.Router();
 
 router.use('/api/post', post);
+router.use('/api/comment', comment);
 router.use('/api/login', login);
 router.use('/api/register', register);
 router.use(cors());
