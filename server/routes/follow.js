f093248ac497c405b9db522658d8fd5e6b2b5159
Tigran <Tigrankara@hotmail.com>
2020-03-05 17:43:28 -0500

    fixed follow and mongoose

--- server/routes/follow.js
+++ server/routes/follow.js
@@ -1,16 +1,19 @@
 const User = require("../database/schemas/User");
-const Follow = require("../database/schemas/Follower");
+const { Follow } = require("../database/schemas");
 const express = require("express");
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
 module.exports = router;
 
+const followModel = mongoose.model("Follow")
+
 router.put("/", (req, res) => {
   let follower = req.body.follower;
   let followee = req.body.followee;
 
-  let follow = new Follow({
+  let follow = new followModel({
     follower: follower,
     followee: followee
   });
@@ -37,7 +40,7 @@ router.get("/unfollow", (req, res) => {
   let follower = req.body.follower;
   let followee = req.body.followee;
 
-  Follow.remove({ follower: follower, followee: followee }, (err, result) => {
+  follow.remove({ follower: follower, followee: followee }, (err, result) => {
     if (err) {
       return res.status(404).json({
         success: false,
