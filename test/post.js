15ccd6b9b1b244a4774d0e45da21eea3d777d35c
Sunny <sunnydeng90@gmail.com>
2020-03-04 22:19:43 -0500

    Minor fixes

--- test/post.js
+++ test/post.js
@@ -1,7 +1,7 @@
-var chaiHttp = require("chai-http");
-var chai = require('chai');
-var expect = chai.expect;
-var { server } = require('../server');
+const chaiHttp = require("chai-http");
+const chai = require('chai');
+const expect = chai.expect;
+const server = require('../server');
 
 chai.use(chaiHttp);
 
