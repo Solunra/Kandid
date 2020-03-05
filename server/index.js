6c8518f0a3c5a21c50895c46d1bf0b2d1576399b
Sunny <sunnydeng90@gmail.com>
2020-03-04 22:42:34 -0500

    Test fixes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
-const port = 8000;
+const port = process.env.port || 8000;
 
 require('./database');
 app.use(function(req, res, next) {
--- test/post.js
+++ test/post.js
@@ -5,26 +5,27 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-it("getPostTest", done => {
-    chai.request(server)
-        .get("http://localhost:8000/api/post")
-        .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            expect(res).to.be.json;
-            expect(res).to.include("post");
-            done();
-        });
-});
+describe("Post Tests", () => {
+    it("getPostTest", done => {
+        chai.request(server)
+            .get("http://localhost:8000/api/post")
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                expect(res).to.include("post");
+                done();
+            });
+    });
 
-it("putPostTest", done => {
-    chai.request(server)
-        .put("http://localhost:8000/api/post")
-        .end((err, res) => {
-            expect(err).to.be.null;
-            expect(res).to.have.status(200);
-            expect(res).to.be.json;
-            done();
-        });
+    it("putPostTest", done => {
+        chai.request(server)
+            .put("http://localhost:8000/api/post")
+            .end((err, res) => {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(res).to.be.json;
+                done();
+            });
+    });
 });
-
