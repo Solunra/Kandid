36ff7393855a8ccd08f32819b7245d1f94a07865
Sunny <sunnydeng90@gmail.com>
2020-03-05 23:18:16 -0500

    updated post with followers

--- client/src/components/Wall/Post.js
+++ client/src/components/Wall/Post.js
@@ -6,8 +6,10 @@ import Comment from "../Comment";
 
 export default function Post() {
     const [posts, setPosts] = useState([]);
+    const userEmail = localStorage.getItem("email");
 
     useEffect(() => {request.get('http://localhost:8000/api/post')
+        .query({email: userEmail})
         .then(res => res.body.posts)
         .then(data => {
             setPosts(data);
